e3d55cd927c05775e4e72b0ba81f2471d5ec72db
mat <mat@MyDevice>
2023-10-02 20:23:41 +0200

    added webpack files, edited webpack.config.js, built homepage and implemented css loader

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+       main: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(woff|woff2|ttf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
